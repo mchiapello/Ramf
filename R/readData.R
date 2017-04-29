beebcb170a16d3b87881d2ba0e24fe43dabd5128
Marco Chiapello <mc983.cam@gmail.com>
2017-04-29 04:30:09 +0100

    validation of the dataset

--- R/readData.R
+++ R/readData.R
@@ -18,14 +18,24 @@
 readData <- function(infile){
 	x <- suppressMessages(read_csv(infile))
 	if (dim(x)[2] == 3){
+		### dataset validation
+		if (any(is.na(x))) {
+				stop("The datasets contains NAs")
+		}
+		#         list <- c(rep("character", 2), rep("integer", 5))
+		#         for (i in seq_along(list)){
+		#             if (class(x[[i]]) != list[i]){
+		#                 stop(paste(colnames(x[i]), "column does not contain only", list[i]))
+		#             }
 		class(x) <- c("trouvelot", class(x))
-	}
+		}
 	if (dim(x)[2] == 7){
 		class(x) <- c("grid", class(x))
+		### dataset validation
 	}
 	if (dim(x)[2] != 3 & dim(x)[2] != 7){
 		x <- NULL
-		stop("MARCO")
+		stop("Incorrect dimentions")
 	}
 	return(x)
 }
