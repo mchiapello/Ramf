170485da4984fad350ef731a66980faa15e4c269
Marco Chiapello <mc983.cam@gmail.com>
2017-05-25 11:00:43 +0100

    Try to fix the problem with TRAVIS

--- R/methods.R
+++ R/methods.R
@@ -5,6 +5,7 @@
 #' am_summary(example_grid)
 #' am_summary(example_trouvelot)
 #' @export
+#' @import BiocStyle
 #' @importFrom dplyr group_by
 #' @importFrom dplyr select
 #' @importFrom dplyr summarise
