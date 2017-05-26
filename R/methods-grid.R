3052041fe98a26e89e4ddc4baab32538e73b4c5b
Marco Chiapello <mc983.cam@gmail.com>
2017-05-26 18:40:05 +0100

    Added color and legend to grid plot

--- R/methods-grid.R
+++ R/methods-grid.R
@@ -25,16 +25,13 @@ am_summary.grid <- function(x){
 }
 
 #' @export
-am_barplot.grid <- function(x, ...){
+am_barplot.grid <- function(x, cbPalette = c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"), leg = c("none", "right", "left", "bottom", "top"), ...){
 	Arbuscule <- Hypopodia <- Intr_Hyphae <- Total <- Vesicles <- comp <- NULL
 	features <- replicates <- samples <- values <- NULL
 	# Create summary table
 	y <- grid_summary(x)
 	# Change table shape
 	z <- y %>% tidyr::gather(features, values, -samples, -replicates)
-	# The palette with grey:
-	cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442",
-				   "#0072B2", "#D55E00", "#CC79A7")
 	final <- z %>% group_by(samples, features) %>%
 		  summarize(means = mean(values, na.rm = TRUE),
 					se    = sd(values, na.rm = TRUE))
@@ -53,7 +50,8 @@ am_barplot.grid <- function(x, ...){
 			  panel.grid.major.y = element_blank(),
 			  panel.grid.minor.y = element_blank(),
 			  panel.grid.major.x = element_blank(),
-			  panel.grid.minor.x = element_blank()) +
+			  panel.grid.minor.x = element_blank(),
+			  legend.position = leg[1]) +
 		geom_vline(xintercept = seq(length(unique(z$samples)) + .5, length(unique(z$samples)) * 4 + .5,
 									length(unique(z$samples))), colour = "lightgrey") +
 		labs(title = "Colonization", 
@@ -72,16 +70,13 @@ am_barplot.grid <- function(x, ...){
 }
 
 #' @export
-am_boxplot.grid <- function(x, ...){
+am_boxplot.grid <- function(x, cbPalette = c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"), leg = c("none", "right", "left", "bottom", "top"), ...){
 	Arbuscule <- Hypopodia <- Intr_Hyphae <- Total <- Vesicles <- comp <- NULL
 	features <- replicates <- samples <- values <- NULL
 	# Create summary table
 	y <- grid_summary(x)
 	# Change table shape
 	z <- y %>% tidyr::gather(features, values, -samples, -replicates)
-	# The palette with grey:
-	cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442",
-				   "#0072B2", "#D55E00", "#CC79A7")
 	g <- ggplot(data = z,
 				aes(x = interaction(factor(z$samples, levels = unique(x$samples)),
 									factor(z$features, levels = c("Total", "Hyphopodia",
@@ -98,7 +93,8 @@ am_boxplot.grid <- function(x, ...){
 			  panel.grid.major.y = element_blank(),
 			  panel.grid.minor.y = element_blank(),
 			  panel.grid.major.x = element_blank(),
-			  panel.grid.minor.x = element_blank()) +
+			  panel.grid.minor.x = element_blank(),
+			  legend.position = leg[1]) +
 		geom_vline(xintercept = seq(length(unique(z$samples)) + .5,
 									length(unique(z$samples)) * 4 + .5,
 									length(unique(z$samples))), colour = "lightgrey") +
